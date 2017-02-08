494995e54d3223646a138959f101e972e54c1980
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-02-08 14:45:52 -0800

    Send json data of list of todos.

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/todos', function(req, res) {
+  var todos = [
+    { body: "take out the trash", completed: false },
+    { body: "do the laundry", completed: true }
+  ]
+  res.json(todos);
+});
+
 // Starts a server and listens on port 3000 for connections.
 app.listen(3000, function() {
   console.log('Example app listening on port 3000');
