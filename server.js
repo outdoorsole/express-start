c9afcf317fbbab18ac3771b2eba6295e7978daa3
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-02-08 22:41:02 -0800

    Outline core RESTful routes for todos.

--- server.js
+++ server.js
@@ -9,17 +9,27 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Requests to the root URL (/) or route.
-app.get('/', function (req, res) {
-  var todos = [
-    { body: "take out the trash", completed: false },
-    { body: "do the laundry", completed: true }
-  ]
+// DATA
+var todos = [
+  { body: "take out the trash", completed: false },
+  { body: "do the laundry", completed: true },
+  { body: "make a screencast", completed: false }
+];
 
+// CORE ROUTES
+// 1) TODOS INDEX: Requests to root URL (/) or route.
+app.get('/', function (req, res) {
   res.render('home', { todos: todos });
 });
 
+// TODOS SHOW
+// TODOS CREATE
+// TODOS DELETE
+// TODOS UPDATE
+// TODOS EDIT
+// TODOS NEW
+
 // Starts a server and listens on port 3000 for connections.
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000');
+  console.log('I\'m Alive!');
 })
